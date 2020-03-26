fc1e0615388ef4eb232f36dc439454f670fbbfa1
Ivan Rodrigues <irodrigues@avenuecode.com>
2020-03-26 14:41:29 -0300

    creating api

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+/*
+	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+*/
+//Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	ISBN   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+func getBooks(writer http.ResponseWriter, reader *http.Request) {
+
+}
 
 func main() {
 	fmt.Printf("hello, world\n")
+	//Create router
+	router := mux.NewRouter()
+
+	router.HandleFunc("api/books", getBooks).Methods("GET")
+	router.HandleFunc("api/books", createBook).Methods("POST")
+	router.HandleFunc("api/books{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("api/books{id}", updateBooks).Methods("PUT")
+	router.HandleFunc("api/books{id}", getBooksById).Methods("GET")
+
+	log.Fatal(http.ListenAndServe("8000", router))
+
 }
